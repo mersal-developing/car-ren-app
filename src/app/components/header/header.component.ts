6824847db2a827712af6982a528d7c9f01adc360
mersal.developing@gmail.com <mersal.developing@gmail.com>
2024-12-21 20:05:37 +0400

    eidt header design of profile image

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonIcon, IonButtons, IonToolbar, IonTitle } from "@ionic/angular/standalone";
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { IonHeader, IonIcon, IonButtons, IonToolbar, IonTitle, IonImg } from "@ionic/angular/standalone";
+import { User } from '@supabase/supabase-js';
 import { SearchComponent } from "src/app/components/search/search.component";
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [IonTitle, IonToolbar, IonButtons, IonIcon, IonHeader, SearchComponent]
+  imports: [IonImg, IonTitle, IonToolbar, IonButtons, IonIcon, IonHeader, SearchComponent]
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  private authService = inject(AuthService);
 
-  constructor() { }
+  user: User | null = this.authService.user();
+  userAvatar = signal('');
 
 
+  ngOnInit() {
+    this.userAvatar.set(this.user?.user_metadata['avatar_url']);
+  }
 }
